891ec0332b2413b981e0faaab48ba0255dfe04f7
NiharikaSingh25 <1262253874@mitwpu.edu.in>
2026-04-30 01:22:42 +0530

    Add binary to decimal conversion program

---
+++ program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int binary, decimal = 0, remainder, i = 0;
+  printf("enter a binary no: ");
+    scanf("%d", &binary);
+
+      while (binary != 0) {
+      remainder = binary % 10;
+      decimal += remainder * pow(2,i);
+        binary = binary / 10;
+        i++;
+    }
+  printf("decimal equivalent = %d\n", decimal);
+  return 0;
+}
